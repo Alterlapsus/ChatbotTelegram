4724ac9393e4756dafdb8cc0c9cb3b283e2a1b50
israel <israel.zacarias@plasticosuni.com>
2023-05-25 17:19:44 -0600

    Modificaciones a detatalle y a mostrar resumen_pedido, falta capturar los datos de nombre y edad

--- restauranteBot.py
+++ restauranteBot.py
@@ -2,7 +2,6 @@ from config import *
 import telebot
 from telebot.types import ForceReply
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-from telebot.types import ReplyKeyboardRemove
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 usuarios = {}
@@ -32,35 +31,23 @@ menu = {
 
 @bot.message_handler(commands=["start", "help", "ayuda"])
 def cmd_start(message):
-    markup = ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, "Bienvenido al restaurante:\nUsa el comando /alta para introducir tus datos y tu pedido", reply_markup=markup)
+    markup = InlineKeyboardMarkup(row_width=2)
+    markup.add(InlineKeyboardButton("Agregar nombre", callback_data="agregar_nombre"))
+    markup.add(InlineKeyboardButton("Agregar edad", callback_data="agregar_edad"))
+    bot.send_message(message.chat.id, "Bienvenido al restaurante:\nUsa los botones para agregar tu nombre y edad.",
+                     reply_markup=markup)
 
-@bot.message_handler(commands=['alta'])
-def cmd_alta(message):
-    markup = ForceReply()
-    msg = bot.send_message(message.chat.id, "¿Cómo te llamas?", reply_markup=markup)
-    bot.register_next_step_handler(msg, preguntar_edad)
-
-def preguntar_edad(message):
-    chat_id = message.chat.id
-    nombre = message.text
-    usuarios[chat_id] = {"nombre": nombre}
-    markup = ForceReply()
-    msg = bot.send_message(chat_id, "¿Cuántos años tienes?", reply_markup=markup)
-    bot.register_next_step_handler(msg, guardar_edad)
-
-def guardar_edad(message):
-    chat_id = message.chat.id
-    edad = message.text
-    usuarios[chat_id]["edad"] = edad
-    mostrar_menu_pedido(chat_id)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
     chat_id = call.message.chat.id
     message_id = call.message.message_id
     data = call.data
-    if data == "Finalizar pedido":
+    if data == "agregar_nombre":
+        agregar_nombre(chat_id)
+    elif data == "agregar_edad":
+        agregar_edad(chat_id)
+    elif data == "Finalizar pedido":
         mostrar_resumen_pedido(chat_id, message_id)
         usuarios.pop(chat_id)
     elif data in menu:
@@ -76,13 +63,35 @@ def callback_handler(call):
                 usuarios[chat_id]["pedido"][articulo] = {opcion: 1}
             bot.edit_message_reply_markup(chat_id, message_id, reply_markup=None)
             bot.send_message(chat_id, f"Has agregado {opcion} de {articulo} al pedido.")
-            mostrar_menu_pedido(chat_id)
+            mostrar_menu_pedido(chat_id, usuarios[chat_id].get("nombre"), usuarios[chat_id].get("edad"))
 
-def preguntar_menu_pedido(message):
-    chat_id = message.chat.id
-    mostrar_menu_pedido(chat_id)
 
-def mostrar_menu_pedido(chat_id):
+def agregar_nombre(chat_id):
+    usuarios[chat_id] = {}  # Agregar el ID de chat al diccionario usuarios
+    markup = ForceReply()
+    msg = bot.send_message(chat_id, "¿Cómo te llamas?", reply_markup=markup)
+    bot.register_next_step_handler(msg, guardar_nombre, chat_id)  # Pasamos 'chat_id' como argumento adicional
+
+
+def agregar_edad(chat_id):
+    markup = ForceReply()
+    msg = bot.send_message(chat_id, "¿Cuántos años tienes?", reply_markup=markup)
+    bot.register_next_step_handler(msg, guardar_edad, chat_id)  # Pasamos 'chat_id' como argumento adicional
+
+
+def guardar_nombre(message, chat_id):  # Agregamos 'chat_id' como argumento adicional
+    nombre = message.text
+    usuarios[chat_id]["nombre"] = nombre
+    agregar_edad(chat_id)  # No es necesario retornar 'nombre' aquí
+
+
+def guardar_edad(message, chat_id):  # Agregamos 'chat_id' como argumento adicional
+    edad = message.text
+    usuarios[chat_id]["edad"] = edad
+    mostrar_menu_pedido(chat_id, usuarios[chat_id]["nombre"], usuarios[chat_id]["edad"])  # Pasamos 'nombre' y 'edad' como argumentos
+
+
+def mostrar_menu_pedido(chat_id, nombre, edad):  # Agregamos 'nombre' y 'edad' como argumentos
     markup = InlineKeyboardMarkup(row_width=2)
     for articulo, opciones in menu.items():
         if opciones is None:
@@ -95,13 +104,20 @@ def mostrar_menu_pedido(chat_id):
         reply_markup=markup
     )
 
+
 def mostrar_submenu_pedido(chat_id, articulo):
     opciones = menu.get(articulo, {})
     if opciones:
-        submenu_markup = InlineKeyboardMarkup(row_width=2)
+        markup = InlineKeyboardMarkup(row_width=2)
         for opcion, precio in opciones.items():
-            submenu_markup.add(InlineKeyboardButton(f"{opcion} (Q{precio})", callback_data=f"{articulo}:{opcion}"))
-        bot.send_message(chat_id, f"Elige una opción para {articulo}:", reply_markup=submenu_markup)
+            markup.add(InlineKeyboardButton(f"{opcion} (Q{precio})", callback_data=f"{articulo}:{opcion}"))
+        bot.send_photo(
+            chat_id,
+            open("submenu_image.jpg", "rb"),
+            caption=f"Elige una opción para {articulo}:",
+            reply_markup=markup
+        )
+
 
 def obtener_precio_unitario(articulo, opcion=None):
     if opcion:
@@ -109,15 +125,17 @@ def obtener_precio_unitario(articulo, opcion=None):
     else:
         return menu[articulo]
 
+
 def mostrar_resumen_pedido(chat_id, message_id):
     datos_usuario = usuarios.get(chat_id, {})
     nombre = datos_usuario.get("nombre", "")
     edad = datos_usuario.get("edad", "")
+
     if datos_usuario:
         texto = 'Resumen del pedido:\n\n'
-        texto += f'<code> Nombre:</code> {nombre}\n'
-        texto += f'<code> Edad:</code> {edad}\n'
-        texto += '<code> Pedido:</code>\n'
+        texto += f'<code>Nombre:</code> {datos_usuario}\n'
+        texto += f'<code>Edad:</code> {datos_usuario}\n\n'
+        texto += '<code>Pedido:</code>\n\n'
         total = 0
         for articulo, opciones in datos_usuario.get("pedido", {}).items():
             texto += f'- {articulo}:\n'
@@ -126,13 +144,16 @@ def mostrar_resumen_pedido(chat_id, message_id):
                 subtotal = precio_unitario * cantidad
                 texto += f'  {opcion} (Cantidad: {cantidad}) - Subtotal: Q{subtotal}\n'
                 total += subtotal
-        texto += f'\n<code> Total:</code> Q{int(total)}\n'
-        bot.send_message(chat_id, texto, parse_mode="html")
+        texto += f'\n<code>Total:</code> Q{int(total)}\n'
+        bot.send_message(chat_id, texto, reply_to_message_id=message_id, parse_mode="html")
+
+
 
 
 def obtener_articulo_opcion(data):
     articulo, opcion = data.split(":")
     return articulo, opcion
+
 
 if __name__ == '__main__':
     print('Iniciando el bot...')
